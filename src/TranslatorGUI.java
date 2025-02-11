06ba2d85e8d7f721d51fdfb49fc543f79e10284c
BLMAZ <mazeab25@polyprep.org>
2025-02-11 13:35:18 -0500

    GUI Framework
    Building out GUI, finishing touches and action commands to come.

---
+++ src/TranslatorGUI.java
@@ -0,0 +1,15 @@
+import javax.swing.*;
+
+public class TranslatorGUI {
+    private JButton EXITButton;
+    private JButton switchTranslationModePigButton;
+    private JButton loadFileButton;
+    private JComboBox comboBox1;
+    private JTextPane TextInputPE;
+    private JButton translateButton;
+    private JTextPane textPane1;
+    private JButton saveAsButton;
+    private JFormattedTextField fileNameTxtFormattedTextField;
+
+
+}
